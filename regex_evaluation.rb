0854c1e60f8661b128678b832ce2b638ac921d50
gcmurillo <gcmurillo@outlook.com>
2018-05-24 17:13:06 -0500

    email_evaluation description

--- regex_evaluation.rb
+++ regex_evaluation.rb
@@ -1,8 +1,13 @@
 
-email_regex = "\A[a-z]+\w*(.?\w*)@\b(fiec.|fmcp.)?\bespol.edu.ec$"
-email = "geanmuri@espol.edu.ec"
-m = email.match(/\A[a-z]+\w*(.?\w*)@\b(fiec.|fmcp.)?\bespol.edu.ec$/)
-
+# Crear una función en RUBY que reciba una cadena y retorne un booleano si la cadena
+# corresponde a la estructura de un correo con dominio ESPOL sabiendo que un correo de la
+# institución tiene las siguientes características:
+# a) Su usuario inicia sólo con letras.
+# b) Su usuario solo puede contener letras en minúscula y números.
+# c) Su usuario puede tener puntos y o guiones, pero no de forma continua (…).
+# d) El dominio debe ser espol.edu.ec. Sin embargo, también podría aceptar el subdominio
+# de facultades: fiec.espol.edu.ec, fimcp.espol.edu.ec
+# e) Solo puede tener un @
 
 def email_evaluation(email)
     m = email.match(/\A[a-z]+\w*(.?\w*)@\b(fiec.|fmcp.)?\bespol.edu.ec$/)[0]
